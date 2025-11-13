1d5bf48e50b162aea5f292fafb4bafa3d9dd51fd
PrinceMuichkine <babacar.diop.bba@edhec.com>
2025-11-13 12:57:43 +0000

    Add Google One Tap

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -1,7 +1,13 @@
 /* @proprietary license */
 
 import { HomePageClient } from '@/components/home/home-page-client';
+import { GoogleOneTap } from '@/components/home/google-one-tap';
 
 export default function Page() {
-  return <HomePageClient />;
+  return (
+    <>
+      <GoogleOneTap />
+      <HomePageClient />
+    </>
+  );
 }
